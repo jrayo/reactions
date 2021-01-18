b2866db1ba8baa5fcd75e6fdc8278169acfc63ef
Juan Felipe <jfrayop@gmail.com>
2021-01-18 14:41:22 -0500

    Added different timeouts for static regeneration

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,5 @@
 import Head from 'next/head'
-
-export async function getStaticProps() {
-  // {
-  //   "data": {
-  //     "repository": {
-  //       "issue": {
-  //         "reactionGroups": [
-  //           { "content": "THUMBS_UP", "users": { "totalCount": 0 } },
-  //           { "content": "THUMBS_DOWN", "users": { "totalCount": 0 } },
-  //           { "content": "LAUGH", "users": { "totalCount": 0 } },
-  //           { "content": "HOORAY", "users": { "totalCount": 0 } },
-  //           { "content": "CONFUSED", "users": { "totalCount": 0 } },
-  //           { "content": "HEART", "users": { "totalCount": 0 } },
-  //           { "content": "ROCKET", "users": { "totalCount": 0 } },
-  //           { "content": "EYES", "users": { "totalCount": 0 } }
-  //         ]
-  //       }
-  //     }
-  //   }
-  // }
-  const res = await fetch('https://api.github.com/graphql', {
-    method: 'POST',
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`
-    },
-    body: JSON.stringify({
-      query: `query {
-        repository(owner:"chibicode", name:"reactions") {
-          issue(number:1) {
-            reactionGroups {
-              content
-              users(first: 0) {
-                totalCount
-              }
-            }
-          }
-        }
-      }`
-    })
-  })
-
-  const json = await res.json()
-  if (res.status !== 200) {
-    console.error(json)
-    throw new Error('Failed to fetch API')
-  }
-
-  // [0, 0, 0, 0, 0, 0, 0, 0]
-  const reactions = json.data.repository.issue.reactionGroups.map(
-    (item) => item.users.totalCount
-  )
-
-  return {
-    props: {
-      reactions
-    },
-    revalidate: 1
-  }
-}
-
+import Link from 'next/link'
 export default function Home({ reactions }) {
   return (
     <div className='container'>
@@ -84,75 +25,38 @@ export default function Home({ reactions }) {
           <a href='https://github.com/chibicode/reactions/issues/1'>
             this GitHub issue
           </a>
-          :
+          
+
         </h3>
-        <div className='line'>
-          <span className='emoji'>👍</span> <strong>{reactions[0]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>👎</span> <strong>{reactions[1]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>😄</span> <strong>{reactions[2]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>🎉</span> <strong>{reactions[3]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>😕</span> <strong>{reactions[4]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>🧡</span> <strong>{reactions[5]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>🚀</span> <strong>{reactions[6]}</strong>
-        </div>
-        <div className='line'>
-          <span className='emoji'>👀</span> <strong>{reactions[7]}</strong>
-        </div>
-        <br />
-        <div>
-          <strong>Explanation:</strong> This page is statically generated with <a href="https://nextjs.org/">Next.js</a> by fetching data from GitHub. It’s deployed to{' '}
-          <a href='https://vercel.com/docs/v2/edge-network/overview'>Vercel’s Edge Network</a>{' '}
-          (CDN). Importantly, this page is being re-generated using{' '}
-          <a href='https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration'>
-            Incremental Static Regeneration
-          </a> (released in <a href="https://nextjs.org/blog/next-9-5">Next.js 9.5</a>). Here’s how it works:
-        </div>
+          
+          
+          <b>
+            This is a fork from <a href='https://github.com/chibicode/reactions/blob/master/pages/index.js'>@chibicode </a> in order to see other timeouts when statically generating pages.  
+          </b>
+        
+
         <ol>
           <li>
-            Each Next.js page can define the timeout. For this page, it’s set at 1 second.
+            <Link href="/1">
+             <a>1 second</a>
+            </Link>
           </li>
           <li>
-            When a new request comes in, the statically generated page is
-            served.
+            <Link href="/10">
+             <a>10 seconds</a>
+            </Link>
           </li>
           <li>
-            Later, when another request comes in <strong>after the defined timeout is exceeded</strong>:
-            (1) The statically generated page is served, and (2){' '}
-            <strong>
-              Next.js generates a new version of the page in the background and
-              updates the static page for *future* requests
-            </strong>
-            .
+            <Link href="/60">
+             <a>60 seconds</a>
+            </Link>
           </li>
           <li>
-            Later, when another request comes in <strong>after the regeneration is done</strong>: The updated static page is served.
-          </li>
-          <li>
-            This allows Incremental Static Regeneration on a per-page basis
-            without rebuilding the full app. It’ll always be fast because users will always get a static response. <a href='https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration'>
-              Learn more here
-            </a>
-            .
+            <Link href="/customized">
+             <a>{process.env.NEXT_PUBLIC_REVALIDATE_TIMEOUT} seconds (Customized via environment variable)</a>
+            </Link>
           </li>
         </ol>
-        <div>
-          <strong>Source:</strong>{' '}
-          <a href='https://github.com/chibicode/reactions/blob/master/pages/index.js'>
-            pages/index.js
-          </a> - `getStaticProps()` fetches the data during static generation, and `revalidate` specifies the timeout.
-        </div>
       </main>
     </div>
   )
